52f3236391da2d44369db58a68e00100a745f259
manny-uncharted <omoebun52@gmail.com>
2023-01-21 15:29:15 +0100

    done with learning about formatted print

--- Rust-by-example/HelloWorld/src/main.rs
+++ Rust-by-example/HelloWorld/src/main.rs
@@ -29,13 +29,34 @@ fn main() {
         subject="the quick brown fox",
         verb="jumps over");
 
-        // Different formatting can be invoked by specifying the format charcater after ':'
-        println!("Base 10: {} ", 69420);
-        println!("Base 2 (binary): {:b} ", 69420);
-        println!("Base 8 (octal): {:o} ", 69420);
-        println!("Base 16 (hex): {:x} ", 69420);
-        println!("Base 16 (hex): {:X} ", 69420);
+    // Different formatting can be invoked by specifying the format charcater after ':'
+    println!("Base 10: {} ", 69420);
+    println!("Base 2 (binary): {:b} ", 69420);
+    println!("Base 8 (octal): {:o} ", 69420);
+    println!("Base 16 (hex): {:x} ", 69420);
+    println!("Base 16 (hex): {:X} ", 69420);
 
+    // You can right-align text with a specified width. This will output "     1". (four white spaces and a "1", for a total width of 5)
+    println!(" {number:>5} ", number=1);
+
+    // You can even use named arguments in the format specifier by appending a '$' to the argument name.
+    println!(" {number:0>width$} ", number=1, width=5);
+
+    // Rust even checks to make sure the correct number of arguments are used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // Only types that implement fmt:Display can be formatted with '{}'. User defined types require more complicated handling and do not implement fmt:Display by default.
+
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement `fmt::Display`.
+    // println!("This struct `{}` won't print...", Structure(3));
+
+    // For rust 1.58 nd above, you can directly capture the argument from a surrounding variable. Just like the above, this will output  "     1". (four white spaces and a "1", for a total width of 5)
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!(" {number:>width$} ");
 }
 
 // Regular comments are written like this
