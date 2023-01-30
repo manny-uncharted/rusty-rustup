a6b7b49eb5d40582c1ff522f882fb262fb498e39
manny-uncharted <omoebun52@gmail.com>
2023-01-30 09:40:35 +0100

    learning about the testcase:list

--- Rust-by-example/HelloWorld/src/main.rs
+++ Rust-by-example/HelloWorld/src/main.rs
@@ -164,78 +164,87 @@ and extend beyond a single line */
 // This is not a problem though because for any new container type which is not generic,fmt::Display can be implemented.
 
 
-use std::fmt; // Import 'fmt'
+// use std::fmt; // Import 'fmt'
 
-// A structure holding two numbers. 'Debug' will be derived so the results can be contrasted with 'Display'.
-#[derive(Debug)]
-struct MinMax(i64, i64);
+// // A structure holding two numbers. 'Debug' will be derived so the results can be contrasted with 'Display'.
+// #[derive(Debug)]
+// struct MinMax(i64, i64);
 
-// Implement 'Display' for 'MinMax'.
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Use 'self.number to refer to each positional data point.
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
+// // Implement 'Display' for 'MinMax'.
+// impl fmt::Display for MinMax {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Use 'self.number to refer to each positional data point.
+//         write!(f, "({}, {})", self.0, self.1)
+//     }
+// }
 
-// Define a structure where the fields are nameable for comparison.
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
+// // Define a structure where the fields are nameable for comparison.
+// #[derive(Debug)]
+// struct Point2D {
+//     x: f64,
+//     y: f64,
+// }
 
-// Similarly, implement 'Display' for 'Point2D'.
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only 'x' and 'y' are denoted.
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
+// // Similarly, implement 'Display' for 'Point2D'.
+// impl fmt::Display for Point2D {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Customize so only 'x' and 'y' are denoted.
+//         write!(f, "x: {}, y: {}", self.x, self.y)
+//     }
+// }
 
-// Complex struct
-#[derive(Debug)] 
-struct Complex {
-    real: f64,
-    imaginary: f64,
-}
+// // Complex struct
+// #[derive(Debug)] 
+// struct Complex {
+//     real: f64,
+//     imaginary: f64,
+// }
 
-impl fmt::Display for Complex {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only 'real' and 'imaginary' are denoted.
-        write!(f, "real: {}, imaginary: {}i", self.real, self.imaginary)
-    }
-}
+// impl fmt::Display for Complex {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Customize so only 'real' and 'imaginary' are denoted.
+//         write!(f, "real: {}, imaginary: {}i", self.real, self.imaginary)
+//     }
+// }
 
-fn main() {
-    let minmax = MinMax(0, 14);
+// fn main() {
+//     let minmax = MinMax(0, 14);
 
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug {:?}", minmax);
+//     println!("Compare structures:");
+//     println!("Display: {}", minmax);
+//     println!("Debug {:?}", minmax);
 
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
+//     let big_range = MinMax(-300, 300);
+//     let small_range = MinMax(-3, 3);
 
-    println!("The big range is {big} and the small is {small}",
-        small = small_range,
-        big = big_range);
+//     println!("The big range is {big} and the small is {small}",
+//         small = small_range,
+//         big = big_range);
 
-    let point = Point2D { x: 3.3, y: 7.2 };
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
+//     let point = Point2D { x: 3.3, y: 7.2 };
+//     println!("Compare points:");
+//     println!("Display: {}", point);
+//     println!("Debug: {:?}", point);
 
-    // Error, Both `Debug` and `Display` were implemented but `{:b}` requires `fmt::Binary` to be implemented. This will not work.
-    // println!("What does Point2D look like in binary: {:b}?", point);
+//     // Error, Both `Debug` and `Display` were implemented but `{:b}` requires `fmt::Binary` to be implemented. This will not work.
+//     // println!("What does Point2D look like in binary: {:b}?", point);
 
-    // So, fmt::Display has been implemented but fmt::Binary has not, and therefore cannot be used. std::fmt has many such traits and each requires its own implementation. This is detailed further in std::fmt.
+//     // So, fmt::Display has been implemented but fmt::Binary has not, and therefore cannot be used. std::fmt has many such traits and each requires its own implementation. This is detailed further in std::fmt.
 
-    // Activity 
-    // After checking the output of the above example, use the 'Point2D' struct as a guide to add a Complex struct to the example. when printed in the same way.
-    let complex = Complex { real: 3.3, imaginary: 7.2};
-    println!("Compare complex numbers:");
-    println!("Display: {}", complex);
-    println!("Debug: {:?}", complex);
+//     // Activity 
+//     // After checking the output of the above example, use the 'Point2D' struct as a guide to add a Complex struct to the example. when printed in the same way.
+//     let complex = Complex { real: 3.3, imaginary: 7.2};
+//     println!("Compare complex numbers:");
+//     println!("Display: {}", complex);
+//     println!("Debug: {:?}", complex);
 
-}
+// }
+
+
+
+/**
+ * Testcase: List
+ * Implementing fmt::Display for a structure where the elements must each be handled sequentially is tricky. The problem is that each write! generates a fmt::Result. Proper handling of this requires dealing with all the results. Rust provides the ? operator for exactly this purpose.
+Using ? on write! looks like this:
+ * 
+ */
