780177f00839f43af58a26c2736b7d2542eeb6e2
manny-uncharted <omoebun52@gmail.com>
2023-01-31 19:11:19 +0100

    implemented the list case

--- Rust-by-example/HelloWorld/src/main.rs
+++ Rust-by-example/HelloWorld/src/main.rs
@@ -248,3 +248,32 @@ and extend beyond a single line */
 Using ? on write! looks like this:
  * 
  */
+
+ use std::fmt; // Import the 'fmt' module
+
+ // Define a structure named 'list' containing a 'vec'
+ struct List(Vec<i32>);
+
+ impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing, and create a reference to 'vec'
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over 'v' in 'vec' while enumerating the iteration count in 'count'
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma. Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value
+        write!(f, "]")
+    }
+ }
+
+ fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+ }
