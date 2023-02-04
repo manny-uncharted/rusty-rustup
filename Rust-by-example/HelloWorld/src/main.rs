79ecfed67c27d6b7abd27a330857e6c1aeac90bd
manny-uncharted <omoebun52@gmail.com>
2023-02-04 22:55:50 +0100

    using primitives

--- Rust-by-example/HelloWorld/src/main.rs
+++ Rust-by-example/HelloWorld/src/main.rs
@@ -291,51 +291,80 @@ Using ? on write! looks like this:
  
  */
 
-use std::fmt::{self, Formatter, Display};
+// use std::fmt::{self, Formatter, Display};
 
-struct City {
-    name: &'static str,
-    // Latitude
-    lat: f32,
-    // Longitude
-    lon: f32,
-}
+// struct City {
+//     name: &'static str,
+//     // Latitude
+//     lat: f32,
+//     // Longitude
+//     lon: f32,
+// }
 
-impl Display for City {
-    // `f` is a buffer, and this method must write the formatted string into it
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
-        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+// impl Display for City {
+//     // `f` is a buffer, and this method must write the formatted string into it
+//     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+//         let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+//         let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-        // `write!` is like `format!`, but it will write the formatted string
-        // into a buffer (the first argument)
-        write!(f, "{}: {:.3}°{} {:.3}°{}",
-            self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
-    }
-}
+//         // `write!` is like `format!`, but it will write the formatted string
+//         // into a buffer (the first argument)
+//         write!(f, "{}: {:.3}°{} {:.3}°{}",
+//             self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+//     }
+// }
 
-#[derive(Debug)]
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
+// #[derive(Debug)]
+// struct Color {
+//     red: u8,
+//     green: u8,
+//     blue: u8,
+// }
  
+// fn main() {
+//     for city in [
+//         City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+//         City { name: "Oslo", lat: 59.95, lon: 10.75 },
+//         City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+//     ].iter() {
+//         println!("{}", *city);
+//     }
+//     for color in [
+//         Color { red: 128, green: 255, blue: 90 },
+//         Color { red: 0, green: 3, blue: 254 },
+//         Color { red: 0, green: 0, blue: 0 },
+//     ].iter() {
+//         // Switch this to use {} once you've added an implementation
+//         // for fmt::Display.
+//         println!("{:?}", color);
+//     }
+// }
+
+/*
+    Primitives
+
+    Rust provides access to a wide variety of primitives. A sample includes:
+
+    Scalar Types
+    signed integers: i8, i16, i32, i64, i128 and isize (pointer size)
+    unsigned integers: u8, u16, u32, u64, u128 and usize (pointer size)
+    floating point: f32, f64
+    char Unicode scalar values like 'a', 'α' and '∞' (4 bytes each)
+    bool either true or false
+    and the unit type (), whose only possible value is an empty tuple: ()
+    Despite the value of a unit type being a tuple, it is not considered a compound type because it does not contain multiple values.
+
+    Compound Types
+    arrays like [1, 2, 3]
+    tuples like (1, true)
+    Variables can always be type annotated. Numbers may additionally be annotated via a suffix or by default. Integers default to i32 and floats to f64. Note that Rust can also infer types from context.
+
+*/
+
+
 fn main() {
-    for city in [
-        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
-        City { name: "Oslo", lat: 59.95, lon: 10.75 },
-        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
-    ].iter() {
-        println!("{}", *city);
-    }
-    for color in [
-        Color { red: 128, green: 255, blue: 90 },
-        Color { red: 0, green: 3, blue: 254 },
-        Color { red: 0, green: 0, blue: 0 },
-    ].iter() {
-        // Switch this to use {} once you've added an implementation
-        // for fmt::Display.
-        println!("{:?}", color);
-    }
+    // Variables can be type annotated.
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0; // Regular annotation
 }
